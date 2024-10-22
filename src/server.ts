f9fca84cf300d30e06beeedb9c2f33e5f2910645
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-10-22 21:50:11 +0200

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const server = express()
+
+// routing
+server.get('/', (req, res) => {
+
+    res.json('desde get')
+})
+
+server.post('/', (req, res) => {
+
+    res.json('desde post')
+})
+
+server.put('/', (req, res) => {
+
+    res.json('desde put')
+})
+
+server.patch('/', (req, res) => {
+
+    res.json('desde patch')
+})
+
+server.delete('/', (req, res) => {
+
+    res.json('desde delete')
+})
+
+export default server
