6610c020576b7cfe576d7b69503290e89550fcfa
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-11-03 14:17:18 +0100

    Adding more API documentation

--- src/router.ts
+++ src/router.ts
@@ -4,9 +4,78 @@ import { handleInputErrors } from './middleware/index.ts'
 import { createProduct, getProducts, getProductById, updateProduct, updateAvailability, deleteProduct } from './handlers/product.ts'
 
 const router = Router()
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          Product:
+ *              type: object
+ *              properties: 
+ *                  id:
+ *                      type: integer
+ *                      description: The Product ID
+ *                      example: 1
+ *                  name:
+ *                      type: string
+ *                      description: The Product name
+ *                      example: Monitor Curvo de 49 Pulgadas
+ *                  price:
+ *                      type: number
+ *                      description: The Product price
+ *                      example: 300
+ *                  availability: 
+ *                      type: boolean
+ *                      description: The Product availability
+ *                      example: true
+ */
 
-// routing
+/**
+ * @swagger
+ * /api/products:
+ *  get:
+ *      summary: Get a list of products
+ *      tags:
+ *          - Products
+ *      description: Return a list of products
+ *      responses:
+ *          200:
+ *              description: Sucessful response
+ *              content:
+ *                  application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Product'
+ */
 router.get('/', getProducts)
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *      summary: Get a product by ID
+ *      tags: 
+ *          - Products
+ *      description: Return a product based on its unique ID
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            description: The ID of the product to retrieve
+ *            required: true
+ *            schema:
+ *                type: integer
+ *      responses:
+ *          200:
+ *              description: Succesful Response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          404:
+ *              description: Product Not Found
+ *          400:
+ *              description: Bad Request - Invalid ID
+ */
 
 router.get('/:id',
     param('id').isInt().withMessage('ID not valid'),
@@ -14,6 +83,30 @@ router.get('/:id',
     getProductById
 )
 
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Create a new Product
+ *     tags:
+ *       - Products
+ *     description: Returns a new product record in the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Monitor Curvo 49 Pulgadas"
+ *               price:
+ *                 type: number
+ *                 example: 300
+ */
+
+ 
 router.post('/', 
     
     // validation
